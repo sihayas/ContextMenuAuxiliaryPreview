9ec5aacbeb9ab98918cdae5d6887a84ba73cc3f2
cora type <coratype@gmail.com>
2024-11-03 11:33:22 -0500

    Changed to use local DGSwiftUtilities dependency

--- Package.swift
+++ Package.swift
@@ -3,35 +3,35 @@
 import PackageDescription
 
 let package = Package(
-  name: "ContextMenuAuxiliaryPreview",
-  platforms: [
-    .iOS(.v13),
-  ],
-  products: [
-    .library(
-      name: "ContextMenuAuxiliaryPreview",
-      targets: ["ContextMenuAuxiliaryPreview"]
-    ),
-  ],
-  dependencies: [
-    .package(
-      url: "https://github.com/dominicstop/DGSwiftUtilities",
-      .upToNextMajor(from: "0.29.0")
-    ),
-  ],
-  targets: [
-    .target(
-      name: "ContextMenuAuxiliaryPreview",
-      dependencies: [
-        "DGSwiftUtilities"
-      ],
-      path: "Sources",
-      linkerSettings: [
-				.linkedFramework("UIKit"),
-			]
-    ),
-  ],
-  swiftLanguageVersions: [
-    .v5,
-  ]
-);
+    name: "ContextMenuAuxiliaryPreview",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "ContextMenuAuxiliaryPreview",
+            targets: ["ContextMenuAuxiliaryPreview"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            name: "DGSwiftUtilities",
+            path: "../DGSwiftUtilities"
+        ),
+    ],
+    targets: [
+        .target(
+            name: "ContextMenuAuxiliaryPreview",
+            dependencies: [
+                "DGSwiftUtilities",
+            ],
+            path: "Sources",
+            linkerSettings: [
+                .linkedFramework("UIKit"),
+            ]
+        ),
+    ],
+    swiftLanguageVersions: [
+        .v5,
+    ]
+)
